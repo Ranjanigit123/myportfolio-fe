4fc9a6b23eac3158edf2f5964d909646286e5e6e
Ranjanigit123 <ranjanirithu206@gmail.com>
2024-10-16 23:22:02 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//import { HomeComponent } from './home/home.component'; // adjust the path based on your project structure
+//import { AboutComponent } from './about/about.component'; // adjust the path based on your project structure
+import { ContactComponent } from './contact/contact.component'; // adjust the path based on your project structure
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', component: HomeComponent },  // Default route (Home)
+  //{ path: 'about', component: AboutComponent },  // Route to About page
+  { path: 'contact', component: ContactComponent },  // Route to Contact page
+  // Add more routes for other components as needed
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
